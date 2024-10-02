af09ce307b0e7fc2ca3bd19d30945a94d5a0eec6
Scott Feichter <scottfeichter@gmail.com>
2024-10-02 10:35:19 -0700

    misc (msg via sh alias)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // let is const by default unless indicate mut for mutable
+
+    io::stdin()
+        .read_line(&mut guess) // & indicates that this is a reference - references are immutable by default so you must include the mut
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess); // {} is a placeholder like ${} in js
 }
